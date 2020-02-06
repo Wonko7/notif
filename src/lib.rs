a4a6cb95919f9942f9d82dc90b95aa929a6fb2bf
William <william@undefined.re>
2020-02-06 12:49:49 +0100

    paste zmq examples as-is

--- src/lib.rs
+++ src/lib.rs
@@ -29,16 +29,71 @@ impl Config {
 }
 
 pub fn run_sender() -> Result<(), failure::Error> {
-    if 1 == 2 {
-        return Err(failure::err_msg("lol"));
+    println!("Sender");
+    let context = zmq::Context::new();
+
+    //socket to talk to clients
+    let publisher = context.socket(zmq::PUB).unwrap();
+    publisher.set_sndhwm(1_100_000).expect("failed setting hwm");
+    publisher
+        .bind("tcp://*:5561")
+        .expect("failed binding publisher");
+
+    //socket to receive signals
+    let syncservice = context.socket(zmq::REP).unwrap();
+    syncservice
+        .bind("tcp://*:5562")
+        .expect("failed binding syncservice");
+
+    //get syncronization from subscribers
+    println!("Waiting for subscribers");
+    for _ in 0..10 {
+        syncservice.recv_msg(0).expect("failed receiving sync");
+        syncservice.send("", 0).expect("failed sending sync");
     }
-    println!("sender");
-    Ok(())
+    //now broadcast 1M updates followed by end
+    println!("Broadcasting messages");
+    for _ in 0..1_000_000 {
+        publisher.send("Rhubarb", 0).expect("failed broadcasting");
+    }
 }
 
 pub fn run_aggregator() -> Result<(), failure::Error> {
-    println!("sender");
-    Ok(())
+    println!("Aggregator");
+
+    let context = zmq::Context::new();
+
+    //first connect our subscriber
+    let subscriber = context.socket(zmq::SUB).unwrap();
+    subscriber
+        .connect("tcp://localhost:5561")
+        .expect("failed connecting subscriber");
+    subscriber
+        .set_subscribe(b"")
+        .expect("failed setting subscription");
+    thread::sleep(Duration::from_millis(1));
+
+    //second sync with publisher
+    let syncclient = context.socket(zmq::REQ).unwrap();
+    syncclient
+        .connect("tcp://localhost:5562")
+        .expect("failed connect syncclient");
+    syncclient.send("", 0).expect("failed sending sync request");
+    syncclient.recv_msg(0).expect("failed receiving sync reply");
+
+    //third get our updates and report how many we got
+    let mut update_nbr = 0;
+    loop {
+        let message = subscriber
+            .recv_string(0)
+            .expect("failed receiving update")
+            .unwrap();
+        if message == "END" {
+            break;
+        }
+        update_nbr += 1;
+    }
+    println!("Received {} updates", update_nbr);
 }
 
 pub fn run_sink() -> Result<(), failure::Error> {
