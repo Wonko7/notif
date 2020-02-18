0fae6fc979ee25b610214960e550604da849ae4b
William <william@undefined.re>
2020-02-18 21:40:08 +0100

    WIP: sender: add interrupt PoC

--- src/run.rs
+++ src/run.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::config::Config;
 use crate::notif::Notification;
 
@@ -7,7 +9,7 @@ use signal_hook::{iterator::Signals, SIGHUP};
 
 
 pub fn send(config: Config, hostname: &str, summary: &str, body: &str, urgency: &str) -> Result<(), failure::Error> {
-    let _              = config.auth.build()?;
+    let _          = config.auth.build()?;
     let send_notif = config.sender.build()?; // better config FIXME
 
     let msg = Notification {
@@ -72,10 +74,22 @@ pub fn route(config: Config) -> Result<(), failure::Error> {
 
 pub fn notify(config: Config, hostname: &str, id: String) -> Result<(), failure::Error> {
     println!("notify");
-    let _            = config.auth.build()?;
-    let incoming_notif = config.notifier.build()?; // connect.
+    let _                = config.auth.build()?;
+    let incoming_notif   = config.notifier.build()?; // connect.
+    let interrupted      = Arc::new(false);
+    let mut interrupted2 = Arc::clone(&interrupted);
 
     incoming_notif.send("seize!")?;
+
+    // catch SIGHUP to seize notifier! (use on unlock xscreensaver, etc)
+    let signals        = Signals::new(&[SIGHUP])?;
+    std::thread::spawn(move || {
+        for _signal in signals.forever() {
+            println!("seizing!");
+            *Arc::get_mut(&mut interrupted2).unwrap() = true;
+            // set global to one, check it in else
+        }
+    });
 
     let mut poller     = Poller::new();
     let mut events     = Events::new();
@@ -84,79 +98,39 @@ pub fn notify(config: Config, hostname: &str, id: String) -> Result<(), failure:
 
     loop {
         println!("waiting");
-        poller.poll(&mut events, Period::Infinite)?;
+        if let Ok(_) = poller.poll(&mut events, Period::Infinite) {
+            for event in &events {
+                // event.is_readable()), .id = PollId(0), unwrap a result?
+                if event.is_readable() {
+                    let msg = incoming_notif.recv_msg()?;
+                    println!("len: {}", msg.len());
 
-        for event in &events {
-            // event.is_readable()), .id = PollId(0), unwrap a result?
-            if event.is_readable() {
-                let msg = incoming_notif.recv_msg()?;
-                println!("len: {}", msg.len());
+                    if msg.len() == 3 && msg.to_str()? == "ACK" {
+                        println!("ignore ack");
+                        continue;
+                    }
+                    let notif: Notification = bincode::deserialize(&msg.as_bytes())?;
+                    println!("so easy {:?}", notif);
 
-                if msg.len() == 3 && msg.to_str()? == "ACK" {
-                    continue;
+                    let keep_in_scope: String;
+                    let summary = if notif.hostname != hostname {
+                        keep_in_scope = format!("@{}: {}", notif.hostname, notif.summary);
+                        keep_in_scope.as_str()
+                    } else {
+                        notif.summary
+                    };
+
+                    std::process::Command::new("/usr/bin/notify-send")
+                        .args(&["-u", notif.urgency, "--", summary, notif.body])
+                        .spawn()?
+                        .wait()?;
+                } else if *interrupted {
+                    println!("else... int");
+                    // check interrupt global here.
                 }
-                let notif: Notification = bincode::deserialize(&msg.as_bytes())?;
-                println!("so easy {:?}", notif);
             }
+        } else {
+            println!("int");
         }
     }
-
-    // let ack = notif_router.recv_msg()?; // we need prelude::*, why?
-
-    // loop {
-    //     let request = server.recv_msg()?; // we need prelude::*, why?
-
-    //     // Retrieve the routing_id to route the reply to the client.
-    //     let id = request.routing_id().unwrap();
-    //     println!("got {:?}: {}", id, request.to_str()?);
-    //     server.route("pong", id)?;
-    // }
-    // println!("notify");
-    // Ok(())
-    // let context        = zmq::Context::new();
-    // let seize_notifier = context.socket(zmq::REQ)?;
-    // let incoming_notif = context.socket(zmq::SUB)?;
-
-    // seize_notifier.connect(format!("tcp://{}:{}", config.server_ip, config.notifier_seize_port).as_str())?;
-    // seize_notifier.send(id.as_str(), 0)?;
-    // incoming_notif.connect(format!("tcp://{}:{}", config.server_ip, config.outgoing_notif_port).as_str())?;
-    // incoming_notif.set_subscribe(id.as_bytes())?;
-    // seize_notifier.recv_string(0)?.unwrap(); // wait for ack.
-
-    // // catch SIGHUP to seize notifier! (use on unlock xscreensaver, etc)
-    // let signals        = Signals::new(&[SIGHUP])?;
-    // std::thread::spawn(move || {
-    //     for _signal in signals.forever() {
-    //         seize_notifier.send(id.as_str(), 0).expect("could not send seize_notifier");
-    //         seize_notifier.recv_string(0).expect("could not recv seize_notifier").unwrap();
-    //         println!("Seized notifier as {}", id.as_str());
-    //     }
-    // });
-
-    // loop {
-    //     if let Ok(notif) = incoming_notif.recv_multipart(0) { // ignore Err, interrupts for example.
-    //         if notif.len() != 5 {
-    //             println!("Dropping message with {} parts", notif.len());
-    //             continue;
-    //         }
-
-    //         // put this in a fn and ignore Err, we shouldn't return and exit because of bad utf8.
-    //         let keep_in_scope: String;
-    //         let notif_get      = |i: usize| std::str::from_utf8(&notif[i]);
-    //         let notif_hostname = notif_get(1)?;
-    //         let priority       = notif_get(2)?;
-    //         let body           = notif_get(4)?;
-    //         let title          = if notif_hostname != hostname {
-    //             keep_in_scope = format!("@{}: {}", notif_hostname, notif_get(3)?);
-    //             keep_in_scope.as_str()
-    //         } else {
-    //             notif_get(3)?
-    //         };
-
-    //         std::process::Command::new("/usr/bin/notify-send")
-    //             .args(&["-u", priority, "--", title, body])
-    //             .spawn()?
-    //             .wait()?;
-    //     }
-    // }
 }
