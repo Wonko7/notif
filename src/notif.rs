79d149a43c8db82006108e9a73734e2b9be76275
William <william@undefined.re>
2020-02-18 20:20:48 +0100

    WIP: all three roles compile and PoC work

---
+++ src/notif.rs
@@ -0,0 +1,10 @@
+extern crate serde;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Notification<'a> {
+    pub hostname: &'a str,
+    pub summary:  &'a str,
+    pub body:     &'a str,
+    pub urgency:  &'a str,
+}
